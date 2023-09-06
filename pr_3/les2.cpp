b613e88b0c6b948467697af8e2cae8df2ef14f92
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-09-06 10:00:32 +0300

    Update les2.cpp

--- pr_3/les2.cpp
+++ pr_3/les2.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "RU");
-	/*6-й вариант*/
+	/*6-Р№ РІР°СЂРёР°РЅС‚*/
 	int x; int n = 1; int start = 20;
-	cout << "Введите число x: ";
+	cout << "Р—РЅР°С‡РµРЅРёРµ x: ";
 	cin >> x;
 
 	while (n != start) {
 		int y = (pow(x, 2 * n - 2) / (2 * n + 1));
-		cout << n << ". Значение y: " << y << endl;
+		cout << n << ". Р—РЅР°С‡РµРЅРёРµ y: " << y << endl;
 		n++;
 	}
 }
