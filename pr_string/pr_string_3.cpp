e695c54e328e65f0e55b5ab97e9051e3b67c71e8
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-10-27 15:21:14 +0300

    Update pr_string_3.cpp

--- pr_string/pr_string_3.cpp
+++ pr_string/pr_string_3.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
 #include <string>
-#include <Windows.h>
 
 using namespace std;
 
 int main() {
 
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "ru");
 
 	string test = "1234567890-+*/%**";
-	string text_from_user = "";
-	bool choice = true;
-
-	while (choice == true) {
-		cout << "Введите текст: ";
-		string now_text; cin >> now_text;
-		text_from_user += now_text + " ";
-
-		cout << "Желаете продолжить ввод? (1-Yeas | 0-False): ";
-		cin >> choice;
-	}
+	string text_from_user;
+	getline(cin, text_from_user);
 
 	string result = "";
 	for (int i = 0; i < text_from_user.length(); i++) {
