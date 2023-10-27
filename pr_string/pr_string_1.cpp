3c7b49a81188b6f06037147479a547219e997327
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-10-27 15:20:52 +0300

    Update pr_string_1.cpp

--- pr_string/pr_string_1.cpp
+++ pr_string/pr_string_1.cpp
@@ -1,30 +1,18 @@
 #include <iostream>
-#include <Windows.h>
 
 using namespace std;
 
 int main() {
 
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "ru");
 
-	bool choice = true;
-	string text_user = "";
-	while (choice == true) {
-		cout << "Введите слово: ";
-		string now_t;
-		cin >> now_t;
-		text_user.append(now_t);
-		text_user.append(" ");
-		cout << "Хотите закончить? (1-Yeas; 0-No): ";
-		cin >> choice;
-	}
+	string text_user;
+	getline(cin, text_user);
 	int *array = new int[10];
 	int indx = 0;
-	for (int j = 0; j+1 < text_user.size(); j++) {
+	for (int j = 0; j+1 < text_user.length(); j++) {
 		if (text_user[j] == '.' && text_user[j+1] == ' ') {
 			array[indx] = j+1;
-			cout << text_user[j] << " ";
 			indx++;
 		}
 	}
