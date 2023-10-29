840972a96ff8399552e0054b0736104ce0f7a796
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-10-30 00:53:46 +0300

    Update main.cpp

--- teory/class/class_op/main.cpp
+++ teory/class/class_op/main.cpp
@@ -14,6 +14,7 @@ int main() {
 
 
 	int price_car = 0;
+	string marka_1; string model_1; string gos_nomer_1; int year_1; int price_1;
 	//Создание объекта машин.
 	while (max_car > 0) {
 		string marka; string model; string gos_nomer; int year; int price;
@@ -23,13 +24,14 @@ int main() {
 		cout << "Год: "; cin >> year;
 		cout << "Цена: "; cin >> price;
 		Car* new_car = new Car(marka, model, gos_nomer, year, price);
-		if (new_car->max_price() >= price_car) {
-			price_car = new_car->max_price();
+		if (price_car <= price) {
+			price_car = price;
+			marka_1 = marka; model_1 = model; gos_nomer_1 = gos_nomer_1; year_1 = year; price_1 = price;
 		}
 		new_car->information_about_car();
-		new_car->set_information();
 		max_car--;
 		if (max_car == 0) {
-			new_car->max_price();
+			new_car->max_price(marka_1, model_1, gos_nomer_1, year_1, price_1);
 		}
 	}
+}
