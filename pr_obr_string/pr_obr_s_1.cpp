f555aa2aae139f31363cda3ab3c66cf8b69f6f61
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-10-31 10:23:59 +0300

    Update pr_obr_s_1.cpp

--- pr_obr_string/pr_obr_s_1.cpp
+++ pr_obr_string/pr_obr_s_1.cpp
@@ -1,45 +1,57 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
 #include <string>
+#include <Windows.h>
 
 using namespace std;
 
 int find_word_a(string text);
 int find_words(string text);
+string text_replace(string text);
 
 int main()
 {
-    
-    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
     /*
         6-й вариант. 1-е задание.
         Пользователь вводит текст. Вычислить количество слов начинающихся на А.
-        Количество слов мало или Мало. 
+        Количество слов мало или Мало.
         Заменить в тексте слова доллар на рубль.
     */
-    
+
     string text_user;
     cout << "Введите текст: ";
     getline(cin, text_user);
-    int count = find_words(text_user);
-    cout << "Количество слова на букву А: " << count;
-    text_user.replace(text_user.begin(), text_user.end(), "доллар", "рубль");
-    cout << text_user;
+    text_user.insert(0, " ");
+    int count = find_word_a(text_user);
+    cout << "Количество слова на букву А: " << count << endl;
+    cout << "Количество слов мало: " << find_words(text_user) << endl;
+    string new_str = text_replace(text_user);
+    cout << "Изменённая строка: " << new_str;
+}
+
+string text_replace(string text) {
+    string rubl = "рубль";
+    string dollar = "доллар";
+    int indx = 0;
+
+    for (int j = 0; j < text.length() - 1; j++) {
+        indx = text.find(dollar);
+        if (j == indx) {
+            text.replace(indx, dollar.length(), rubl);
+        }
+    }
+    return text;
 }
 
 
 int find_word_a(string text) {
     int count = 0;
-    for(int i = 0; i < text.length(); i++) {
-        if (text[i-1] == ' ') {
-            if (text[i] == 'А' || text[i] == 'а') {
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] == ' ') {
+            if (text[i + 1] == 'А' || text[i + 1] == 'а') {
                 count++;
             }
         }
@@ -49,20 +61,19 @@ int find_word_a(string text) {
 
 int find_words(string text) {
     int count = 0;
-    for (int j = 0; j < text.length(); j++) {
+    for (int j = 0; j <= text.length(); j++) {
         string word;
-        if (text[j-1] == ' ') {
-            for (int i = j; i < text.length(); i++) {
-                word += text[i];
+        if (text[j] == ' ') {
+            for (int i = (j + 1); i < text.length(); i++) {
                 if (text[i] == ' ') {
-                    if (word.compare("Мало") == 0 || word.compare("мало") == 0) {
+                    if (word == "мало" || word == "Мало") {
                         count++;
                     }
                     break;
                 }
+                word += text[i];
             }
         }
     }
     return count;
 }
-
