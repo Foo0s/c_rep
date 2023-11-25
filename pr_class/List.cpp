31ec1e2433f00a4e2073799647026d921f0a5180
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-11-25 13:25:03 +0300

    Update List.cpp

--- pr_class/List.cpp
+++ pr_class/List.cpp
@@ -12,8 +12,9 @@ List::List(int size_list) {
 	cout << "Массив был успешно создан" << endl;;
 }
 
-List::List(int *array) {
-	this->size_list = sizeof(array);
+List::List(int* array) {
+	this->size_list = sizeof(array) / 2;
+	this->list = 0;
 	this->list = new int[size_list];
 	for (int i = 0; i < size_list; i++) {
 		list[i] = array[i];
@@ -41,9 +42,9 @@ void List::show_list() {
 
 void List::inverse_list() {
 	this->invert = true;
-	int *new_list = new int[sizeof(list)];
+	int* new_list = new int[size_list];
 	int count_id = 0;
-	for (int numb = this->size_list-1; numb >= 0; numb--) {
+	for (int numb = size_list-1; numb >= 0; numb--) {
 		new_list[count_id] = this->list[numb];
 		count_id++;
 	}
@@ -56,18 +57,28 @@ int List::find_unlist(int arr[], int size) {
 	int indx = 0;
 	int count = 0;
 	int countes = 0;
-	for (int el2 = 0; el2 < size_list; el2++) {
-		for (int el = 0; el < size; el++) {
-			if (list[el2] == arr[el]) {
+	int count_2 = 0;
+
+
+	for (int el = 0; el < size_list; el++) {
+		for (int el2 = count; el2 < size; el2++) {
+			if (list[el] == arr[el2]) {
+				indx = el;
 				count++;
-				indx = el2;
 				if (count == size) {
-					return indx;
+					count_2 = count;
 				}
-				continue;
+				break;
+			}
+			else {
+				if (count != 0) {
+					count--;
+				}
 			}
 		}
 	}
-	cout << count << endl;
+	if (count_2 == size) {
+		return indx;
+	}
 	return -1;
 }
